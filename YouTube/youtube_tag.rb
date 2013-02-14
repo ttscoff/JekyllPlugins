73647a514decfd3adb71bce600626a476f7b7d40
Brett Terpstra <me@brettterpstra.com>
2013-02-13 19:17:23 -0600

    Updating YouTube plugin to calculate intrinsic ratio and hardcode CSS

--- YouTube/youtube_tag.rb
+++ YouTube/youtube_tag.rb
@@ -26,9 +26,12 @@ module Jekyll
     def render(context)
       ouptut = super
       if @videoid
+        # Thanks to Andrew Clark for the inline CSS calculation idea <http://contentioninvain.com/2013/02/13/video-embeds-for-responsive-designs/>
+        intrinsic = ((@height.to_f / @width.to_f) * 100)
+        padding_bottom = ("%.2f" % intrinsic).to_s  + "%"
         # remove/comment the next line and adjust the class name on the following line if you already have CSS for responsive video
-        video = "<style>.bt-video-container{position:relative;padding-bottom:56.25%;padding-top:30px;height:0;overflow:hidden}.bt-video-container iframe,.bt-video-container object,.bt-video-container embed{position:absolute;top:0;left:0;width:100%;height:100%;margin-top:0}</style>\n"
-        video += %Q{<div class="bt-video-container"><iframe width="#{@width}" height="#{@height}" src="http://www.youtube.com/embed/#{@videoid}?rel=0" frameborder="0" allowfullscreen></iframe></div>}
+        video = "<style>.bt-video-container iframe,.bt-video-container object,.bt-video-container embed{position:absolute;top:0;left:0;width:100%;height:100%;margin-top:0}</style>\n"
+        video += %Q{<div class="bt-video-container" style="position:relative;padding-bottom:#{padding_bottom};padding-top:30px;height:0;overflow:hidden"><iframe width="#{@width}" height="#{@height}" src="http://www.youtube.com/embed/#{@videoid}?rel=0" frameborder="0" allowfullscreen></iframe></div>}
       else
         "Error processing input, expected syntax: {% youtube video_id [width height] %}"
       end
